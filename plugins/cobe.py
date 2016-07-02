968c5e39bbed217ccbec5249217d1a669fd0102a
sli <signedlongint@gmail.com>
2016-07-01 23:39:31 -0400

    added per-user settings for cobe; moved cobe data files to their own directory; adding brain training feature

--- plugins/cobe.py
+++ plugins/cobe.py
@@ -1,4 +1,6 @@
 import os
+import glob
+import json
 from telepot import glance
 from cobe.brain import Brain
 
@@ -13,14 +15,42 @@ Commands:
 
 class CobePlugin(object):
     def __init__(self):
-        if not os.path.isfile('data/jabberbot.brain'):
-            Brain.init('data/jabberbot.brain')
+        if not os.path.isdir('data/cobe/'):
+            os.mkdir('data/cobe/')
+        if not os.path.isfile('data/cobe/jabberbot.brain'):
+            Brain.init('data/cobe/jabberbot.brain')
 
-        self.brain = Brain('data/jabberbot.brain')
-        self.silent = None
+        self.brain = Brain('data/cobe/jabberbot.brain')
+
+        if not os.path.isfile('data/cobe/lusers'):
+            with open('data/cobe/lusers', 'w') as f:
+                f.write('{}')
+
+        with open('data/cobe/lusers', 'r') as f:
+            self.lusers = json.load(f)
 
     def setup(self, bot):
-        pass
+        if not 'cobe' in bot.config:
+            bot.config['cobe'] = {}
+
+        ccfg = bot.config['cobe']
+        self.silent = ccfg.get('silent', True)
+
+        self._dbg = bot._dbg
+
+        if os.path.isfile('data/cobe/train.txt'):
+            self._dbg('Training file found.' tag='PLUGIN')
+            self.brain.start_batch_learning()
+            with open('data/cobe/train.txt') as f:
+                lines = f.read().split('\n')
+                for line in lines:
+                    self.brain.learn(line)
+            self.brain.stop_batch_learning()
+            t_files = glob.glob('data/cobe/train.txt.*')
+            dst = 'data/cobe/train.txt.{}'.format(len(t_files))
+            os.rename('data/cobe/train.txt', dst)
+            self._dbg('Training complete. Training file moved to {}.'
+                      .format(os.path.basename(dst)))
 
     async def run(self, msg, bot):
         # Don't learn URLs.
@@ -29,18 +59,17 @@ class CobePlugin(object):
                 if e['type'] == 'url':
                     return
 
-        if not self.silent:
-            if 'cobe' in bot.config:
-                ccfg = bot.config.get('cobe', {})
-                self.silent = ccfg.get('silent', True)
-
         self.brain.learn(msg['text'])
 
-        if not self.silent:
+        m_type = msg['chat']['type']
+        u_id = 'id_' + str(msg['from']['id'])
+
+        if not self.silent or \
+           (m_type == 'private' and self.lusers[u_id]):
             content_type, chat_type, chat_id = glance(msg)
             m_id = msg['message_id']
             reply = self.brain.reply(msg['text'])
-            await bot.sendMessage(chat_id, reply, reply_to_message_id=m_id)
+            await bot.sendMessage(chat_id, reply)
 
     async def ask(self, msg, bot):
         content_type, chat_type, chat_id = glance(msg)
@@ -49,20 +78,48 @@ class CobePlugin(object):
         if len(text) > 0:
             self.brain.learn(text)
             reply = self.brain.reply(text)
-        else:
-            reply = 'You didn\'t say anything.'
+            await bot.sendMessage(chat_id, reply, reply_to_message_id=m_id)
 
-        await bot.sendMessage(chat_id, reply, reply_to_message_id=m_id)
+    async def chat(self, msg, bot):
+        content_type, chat_type, chat_id = glance(msg)
+        command = msg['text'].split(' ', 1)[0]
+        args = msg['text'].split(' ')[1:]
+        u_id = 'id_' + str(msg['from']['id'])
+        if len(args) > 0:
+            if args[0] == 'on':
+                self.lusers[u_id] = True
+                reply = 'Chat is: on'
+                await bot.sendMessage(chat_id, reply)
+            elif args[0] == 'off':
+                self.lusers[u_id] = False
+                reply = 'Chat is: off'
+                await bot.sendMessage(chat_id, reply)
+            else:
+                reply = 'Eh? Are you turning chat on or off?'
+                await bot.sendMessage(chat_id, reply)
+        else:
+            p = self.lusers.get(u_id, False)
+            ch = 'on' if p else 'off'
+            reply = 'Chat is: {}'.format(ch)
+            await bot.sendMessage(chat_id, reply)
+
+        print(self.lusers)
+        self._flush()
 
     def get_reply(self, incoming_msg, bot):
         self.brain.learn(incoming_msg)
         reply = self.brain.reply(incoming_msg)
         return reply
 
+    def _flush(self):
+        with open('data/cobe/lusers', 'w') as f:
+            json.dump(self.lusers, f, indent=2)
+
 p = CobePlugin()
 
 exports = {
     'self': p,
     'text': p.run,
-    '/ask': p.ask
+    '/ask': p.ask,
+    '/chat': p.chat
 }
