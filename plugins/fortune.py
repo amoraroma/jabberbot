39199a9a355c53cb617c666e6f8121da7f0d424e
sli <signedlongint@gmail.com>
2016-07-01 20:17:50 -0400

    fortune now detects cowsay on its own

--- plugins/fortune.py
+++ plugins/fortune.py
@@ -1,22 +1,31 @@
 import os
 import glob
 import random
+import platform
 import subprocess
 
 from telepot import glance
 
 __author__ = 'sli'
 __version__ = '0.1'
-__doc__ = '''Tells you your fortune. Available sources are: {}
 
-This fortune might have super cow powers.
+try:
+    subprocess.call(['cowsay'])
+    __doc__ = '''Tells you your fortune. Available sources are: {}
+
+This fortune has super cow powers.
+
+Commands:
+  * /fortune [source] (Example: /fortune {}; /fortune)'''
+except:
+    __doc__ = '''Tells you your fortune. Available sources are: {}
 
 Commands:
   * /fortune [source] (Example: /fortune {}; /fortune)'''
 
 
 class FortunePlugin(object):
-    def __init__(self, enable_cow=False):
+    def __init__(self):
         self.fortunes = {}
         fortune_files = glob.glob('data/fortunes/*')
         for fortune_file in fortune_files:
@@ -27,21 +36,25 @@ class FortunePlugin(object):
         self._sources = list(self.fortunes.keys())
 
     def setup(self, bot):
-        try: 
-            cs_sub = subprocess.Popen(['cowsay', '-l'],
-                                      stdout=subprocess.PIPE,
-                                      stderr=subprocess.PIPE)
-            output, err = cs_sub.communicate()
-            output = output.decode('utf-8').rstrip().split('\n')[1:]
-            output = ' '.join(output)
-            self._cowfigs = output.split(' ')
+        if not 'fortune' in bot.config:
+            bot.config['fortune'] = {}
 
-            if 'fortune' in bot.config:
+        cow = bot.config['fortune'].get('cows', False)
+        if cow:
+            try: 
+                cs_sub = subprocess.Popen(['cowsay', '-l'],
+                                          stdout=subprocess.PIPE,
+                                          stderr=subprocess.PIPE)
+                output, err = cs_sub.communicate()
+                output = output.decode('utf-8').rstrip().split('\n')[1:]
+                output = ' '.join(output)
+                self._cowfigs = output.split(' ')
+
                 dcs = bot.config['fortune'].get('disabled-cows', [])
                 for dc in dcs:
                     self._cowfigs.remove(dc)
-        except:
-            self._cowfigs = []
+            except:
+                self._cowfigs = []
 
     async def run(self, msg, bot):
         content_type, chat_type, chat_id = glance(msg)
