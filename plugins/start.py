cb96a763088bf2a75b850ee16df6b08a729238db
ali4689y <www.zonerteam@gmail.com>
2016-09-15 12:40:19 +0430

    Create start.py

---
+++ plugins/start.py
@@ -0,0 +1,18 @@
+import asyncio
+from message import Message
+
+
+@asyncio.coroutine
+def run(message, matches, chat_id, step):
+    return [Message(chat_id).set_text(
+        "Hi :-) \n*Bot Search Music!*\n✔Fast and accurate✔\n\n_/Music <Artist name>_\n*Creator* = @LullSky", parse_mode="markdown")]
+
+
+plugin = {
+    "name": "About",
+    "desc": "Get Information About The Developer!",
+    "usage": ["/about"],
+    "run": run,
+    "sudo": False,
+    "patterns": ["^[/]start$"]
+}
